5361ec663ebb45ac9e14da9673dfaf9f5323dfac
buptyuxin <buptyuxin@163.com>
2015-04-01 16:44:06 +0800

    解析XML支持嵌套

--- weixin-server/src/main/java/com/yanmo/weixin/domain/BaseKeyValuePairDO.java
+++ weixin-server/src/main/java/com/yanmo/weixin/domain/BaseKeyValuePairDO.java
@@ -1,6 +1,7 @@
 package com.yanmo.weixin.domain;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by yanmo.yx on 2015/3/30.
@@ -10,6 +11,7 @@ public class BaseKeyValuePairDO implements Serializable {
 
     private String key;
     private String value;
+    private List<BaseKeyValuePairDO> subPairs;
 
     public String getKey() {
         return key;
@@ -27,6 +29,14 @@ public class BaseKeyValuePairDO implements Serializable {
         this.value = value;
     }
 
+    public List<BaseKeyValuePairDO> getSubPairs() {
+        return subPairs;
+    }
+
+    public void setSubPairs(List<BaseKeyValuePairDO> subPairs) {
+        this.subPairs = subPairs;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -35,15 +45,24 @@ public class BaseKeyValuePairDO implements Serializable {
         BaseKeyValuePairDO that = (BaseKeyValuePairDO) o;
 
         if (key != null ? !key.equals(that.key) : that.key != null) return false;
+        if (subPairs != null ? !subPairs.equals(that.subPairs) : that.subPairs != null) return false;
         if (value != null ? !value.equals(that.value) : that.value != null) return false;
+
         return true;
     }
 
     @Override
-    public String toString() {
-        return "BaseKeyValuePairDO{" +
-                "key='" + key + '\'' +
-                ", value='" + value + '\'' +
-                '}';
+    public int hashCode() {
+        int result = key != null ? key.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        result = 31 * result + (subPairs != null ? subPairs.hashCode() : 0);
+        return result;
+    }
+
+    public boolean hasSubPairs() {
+        if (subPairs != null && !subPairs.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 }
--- weixin-server/src/main/java/com/yanmo/weixin/utils/XmlUtils.java
+++ weixin-server/src/main/java/com/yanmo/weixin/utils/XmlUtils.java
@@ -1,5 +1,6 @@
 package com.yanmo.weixin.utils;
 
+import com.google.common.collect.Lists;
 import com.yanmo.weixin.domain.BaseKeyValuePairDO;
 import com.yanmo.weixin.domain.MsgDO;
 import org.dom4j.*;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by yanmo.yx on 2015/3/30.
@@ -27,11 +29,8 @@ public class XmlUtils {
             Iterator it = root.elementIterator();
             while (it.hasNext()) {
                 Element element = (Element) it.next();
-                String key = element.getName();
-                String value = element.getText();
                 BaseKeyValuePairDO kv = new BaseKeyValuePairDO();
-                kv.setKey(key);
-                kv.setValue(value);
+                parseElement(element, kv);
                 msgDO.addProperty(kv);
             }
             return msgDO;
@@ -41,13 +40,32 @@ public class XmlUtils {
         return null;
     }
 
+    private static void parseElement(Element element, BaseKeyValuePairDO kv) {
+        List<Element> childs = element.elements();
+        List<BaseKeyValuePairDO> subPairs = kv.getSubPairs();
+        if (!kv.hasSubPairs()) {
+            subPairs = Lists.newArrayList();
+            kv.setSubPairs(subPairs);
+        }
+        if (childs != null && !childs.isEmpty()) {
+            for (Element child : childs) {
+                BaseKeyValuePairDO childKv = new BaseKeyValuePairDO();
+                parseElement(child, childKv);
+                subPairs.add(childKv);
+            }
+        }
+        String key = element.getName();
+        String value = element.getText();
+        kv.setKey(key);
+        kv.setValue(value);
+    }
+
     public static String fromMsg2Xml(MsgDO msgDO) {
         Document doc = DocumentHelper.createDocument();
         Element root = doc.addElement("xml");
+
         for (BaseKeyValuePairDO kv : msgDO.getProperties()) {
-            Element element = root.addElement(kv.getKey());
-            element.addCDATA(kv.getValue());
-//            element.setText(kv.getValue());
+            parseKv(root, kv);
         }
         StringWriter sw = new StringWriter();
         XMLWriter xmlWriter = new XMLWriter(sw, OutputFormat.createPrettyPrint());
@@ -62,6 +80,17 @@ public class XmlUtils {
         return sw.toString();
     }
 
+    private static void parseKv(Element element, BaseKeyValuePairDO kv) {
+        Element child = element.addElement(kv.getKey());
+        if (kv.hasSubPairs()) {
+            for (BaseKeyValuePairDO childKv : kv.getSubPairs()) {
+                parseKv(child, childKv);
+            }
+        } else {
+            child.addCDATA(kv.getValue());
+        }
+    }
+
     public static void main(String[] args) {
         String xml = "<xml>" +
                 " <ToUserName><![CDATA[toUser]]></ToUserName>" +
@@ -71,7 +100,28 @@ public class XmlUtils {
                 " <Content><![CDATA[this is a test]]></Content>" +
                 " <MsgId>1234567890123456</MsgId>" +
                 " </xml>";
-        MsgDO msgDO = fromXml2Msg(xml);
+        String xml2 = "<xml>\n" +
+                "<ToUserName><![CDATA[toUser]]></ToUserName>\n" +
+                "<FromUserName><![CDATA[fromUser]]></FromUserName>\n" +
+                "<CreateTime>12345678</CreateTime>\n" +
+                "<MsgType><![CDATA[news]]></MsgType>\n" +
+                "<ArticleCount>2</ArticleCount>\n" +
+                "<Articles>\n" +
+                "<item>\n" +
+                "<Title><![CDATA[title1]]></Title> \n" +
+                "<Description><![CDATA[description1]]></Description>\n" +
+                "<PicUrl><![CDATA[picurl]]></PicUrl>\n" +
+                "<Url><![CDATA[url]]></Url>\n" +
+                "</item>\n" +
+                "<item>\n" +
+                "<Title><![CDATA[title]]></Title>\n" +
+                "<Description><![CDATA[description]]></Description>\n" +
+                "<PicUrl><![CDATA[picurl]]></PicUrl>\n" +
+                "<Url><![CDATA[url]]></Url>\n" +
+                "</item>\n" +
+                "</Articles>\n" +
+                "</xml> ";
+        MsgDO msgDO = fromXml2Msg(xml2);
         System.out.println(fromMsg2Xml(msgDO));
     }
 }
